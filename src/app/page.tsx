4ebd9bfc81c2c534018b80907142841777fbad00
Fatty Rojas <faty.aome.rojas14@gmail.com>
2024-02-28 14:19:11 -0600

    cap 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  console.log('Hello World')
+
   return (
     <main className={styles.main}>
       <h1>Hello World</h1>
