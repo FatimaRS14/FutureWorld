37cd36fdf8b74a04d1a6170cbc6c8b5b2a4db4f8
Fatty Rojas <faty.aome.rojas14@gmail.com>
2024-03-01 15:05:32 -0600

    2.4 archivos estaticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,6 +1,12 @@
 export const Description = () => {
 
     return(
-        <h1>Descripcion</h1>
+        <section>
+            <img src="/images/description.jpeg" alt="products markeplace" />
+
+            <h2>Description</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque fugiat iusto in illo officiis possimus suscipit perspiciatis dolor, omnis laboriosam cupiditate rem nemo consequuntur at aliquam. Iusto debitis aut enim.</p>
+        </section>
+        
     )
 }
