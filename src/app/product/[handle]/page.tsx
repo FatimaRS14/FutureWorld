4bf7542f29c97ecc2f72761b05fd70a5bbc615ff
Fatty Rojas <faty.aome.rojas14@gmail.com>
2024-04-03 16:39:54 -0600

    4.13 Redirect

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,7 @@
 import { ProductView } from "app/components/product/ProductView"
 import { getProducts } from "app/services/shopify/products"
+import { redirect } from "next/navigation"
+
 
 interface ProductPageProps {
    searchParams: {
@@ -11,6 +13,8 @@ export default async function ProductPage({ searchParams}: ProductPageProps) {
    const id = searchParams.id
    const  products = await getProducts(id)
    const product = products[0]
+
+   
    
   return <ProductView product={product}/> 
 }
