8f8a4a958a7b7386ae852151ec3db960719c8122
Fatty Rojas <faty.aome.rojas14@gmail.com>
2024-04-03 18:47:36 -0600

    4.15 Mejorando SEO

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,11 @@
-import { Description } from "app/components/home/Description";
-import { Hero } from "app/components/home/Hero";
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "✨ Future world",
+  description: "Welcome to the future wold, an ecommerce from other century",                 
+  keywords: ["ecommerce", "future", "world", "technology"],
+}
 
 export default function Home() {
 
